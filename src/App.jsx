662aeea61ce96e96a596e5cf5d8072f080ef8d60
gautamagg938-lang <gautamagg938@gmail.com>
2025-12-18 19:27:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -96,16 +96,16 @@ function App() {
           <h1 id="heroTitle">
             {birthdayReached ? (
               <>
-                Happy Birthday <span className="highlight">[Name]</span> 🎂
+                Happy Birthday <span className="highlight">[Bhavya]</span> 🎂
               </>
             ) : (
               <>
-                Counting down to <span className="highlight">[Name]'s</span>{" "}
+                Counting down to <span className="highlight">[Bhavya]'s</span>{" "}
                 special day 🎂
               </>
             )}
           </h1>
-          <p>Your personalized message goes here 💗</p>
+          <p>A little countdown for you namuni jhalli</p>
         </section>
 
         <Countdown
